2e80a96df62811fa661636624e8686cc04870d01
namratas <namrata.sonatakke@tenerity.com>
2024-02-28 09:42:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading1 = React.createElement("h1",{id:"heading"},"Hellow world in react")
+    const root1 = ReactDOM.createRoot(document.getElementById("root1"))
+    root1.render(heading1) 
